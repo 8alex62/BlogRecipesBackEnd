b0ee426d555da5dd2a3264dc5c6f724031974c8b
matteomarinho <matteo.marinho@estiam.com>
2025-05-28 16:42:53 +0200

    Modification Type Unit dans Ingredient

--- models/IngredientRecipe.js
+++ models/IngredientRecipe.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ingredientRecipeSchema = mongoose.Schema(
     {
-        unit: { type: Number, required: true},
+        unit: { type: String, required: true},
         quantity: { type: Number, required: true},
         Recipe: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipe', required: true },
         Ingredient: { type: mongoose.Schema.Types.ObjectId, ref: 'Ingredient', required: true }
